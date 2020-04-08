356833e43d51f55ca141abe4f2e15ab0acb9c703
ruchika <ruchika6698@gmail.com>
2020-04-08 13:54:57 +0530

    Added:Check the User is Happy or Sad

---
+++ MoodAnalysis/MoodAnalyse.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Mood_Analyzer_Problem
+{
+    class MoodAnalyse
+    {
+        private String message;
+        
+        /// <summary>
+        /// Constructor for mood Message
+        /// </summary>
+        public MoodAnalyse(String message)
+        {
+            this.message = message;
+        }
+
+        public MoodAnalyse()
+        {
+        }
+
+        public string Mood(String message)
+        {
+            this.message = message;
+            return Mood();
+        }
+
+        /// <summary>
+        /// Method to Check Mood Sad or Happy and Exception for Invalid Mood
+        /// </summary>
+        public String Mood()
+        {
+            
+                if (message.Contains("I am in sad mood"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+           
+        }
+
+        /// <summary>
+        /// Main Method
+        /// </summary>
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Welcome to Mood Analyzer Problem");
+           MoodAnalyse moodanalyse = new MoodAnalyse();
+           moodanalyse.Mood("HAPPY");
+        }
+    }
+}
--- TestCases/TestCases.cs
+++ TestCases/TestCases.cs
@@ -1,9 +1,71 @@
-﻿using System;
+﻿using Mood_Analyzer_Problem;
+using NUnit.Framework;
 
-namespace Mood_Analyser_Problem
+namespace TestCases
 {
-    class TestCases
+    [TestFixture]
+    public class TestCases
     {
-        
+        private string message;
+
+        public string String { get; private set; }
+
+        /// <summary>
+        /// Test Case for Sad Mood
+        /// </summary>
+        [Test]
+        public void MoodTestSad()
+        {
+            var moodanalyse = new MoodAnalyse(message);
+            string result = moodanalyse.Mood("I am in sad mood");
+            Assert.AreEqual(result, "SAD");
+        }
+
+        /// <summary>
+        /// Test Case for Happy Mood
+        /// </summary>
+        [Test]
+        public void MoodTestHappy()
+        {
+            var moodanalyse = new MoodAnalyse(message);
+            string result = moodanalyse.Mood("I am in Happy mood");
+            Assert.AreEqual(result, "HAPPY");
+        }
+
+        /// <summary>
+        /// Test Case for Message in Constructor should be sad
+        /// </summary>
+        [Test]
+        public void No_Parameter_Sad()
+        {
+            String = "I am in sad mood";
+            var moodanalyse = new MoodAnalyse(message);
+            string result = moodanalyse.Mood("I am in sad mood");
+            Assert.AreEqual(result, "SAD");
+        }
+
+        /// <summary>
+        /// Test Case for Message in Constructor should be Happy
+        /// </summary>
+        [Test]
+        public void No_Parameter_Happy()
+        {
+            String = "I am in Happy mood";
+            var moodanalyse = new MoodAnalyse(message);
+            string result = moodanalyse.Mood("I am in happy mood");
+            Assert.AreEqual(result, "HAPPY");
+        }
+
+        /// <summary>
+        /// Test Case for Null Mood
+        /// </summary>
+        [Test]
+        public void Exception_Invalid()
+        {
+            var moodanalyse = new MoodAnalyse(null);
+            string result = moodanalyse.Mood("I am in Happy mood");
+            Assert.AreEqual(result, "HAPPY");
+        }
     }
 }
+
